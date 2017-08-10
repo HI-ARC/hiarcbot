b45da712ab60821dddaa29868c77feb64be80a5a
malkoG <rijgndqw012@gmail.com>
2017-08-11 08:19:16 +0900

    Add Emoji with weather report

--- hiarcbot/plugins/weather_report.py
+++ hiarcbot/plugins/weather_report.py
@@ -18,6 +18,24 @@ headers = {
     "appKey": slack_config.WEATHER_PLANET_API_KEY
 }
 
+def weather_status_with_emoji(s):
+    if s=='SKY_M01':
+        return "맑음 :sunny:"
+    elif s=='SKY_M02':
+        return "구름조금 :sun_small_cloud:"
+    elif s=='SKY_M03':
+        return "구름많음 :sun_behind_cloud:"
+    elif s=='SKY_M04':
+        return "흐림 :cloud:" 
+    elif s=='SKY_M05':
+        return "비 :rain_cloud:"
+    elif s=='SKY_M06':
+        return "눈 :snow_cloud:"
+    elif s=='SKY_M07':
+        return "비 또는 눈 :rain_cloud: :snow_cloud:"
+    else:
+        return ":sunglasses:"
+
 def get_weather_summary():
     r = requests.get(url=url, headers=headers)
     if r.status_code != 200:
@@ -26,13 +44,16 @@ def get_weather_summary():
     weather_report = ""
     summary = json.loads(r.text)['weather']['summary'][0]
     weather_report += "[오늘의 날씨]\n(%s / 최저 온도 : %s도 , 최고 온도 : %s도)\n" % \
-                      (summary['today']['sky']['name'], summary['today']['temperature']['tmin'], summary['today']['temperature']['tmax'])
+                      (weather_status_with_emoji(summary['today']['sky']['code']),
+                       summary['today']['temperature']['tmin'], summary['today']['temperature']['tmax'])
 
     weather_report += "[내일의 날씨]\n(%s / 최저 온도 : %s도 , 최고 온도 : %s도)\n" % \
-                      (summary['tomorrow']['sky']['name'], summary['tomorrow']['temperature']['tmin'], summary['tomorrow']['temperature']['tmax'])
+                      (weather_status_with_emoji(summary['tomorrow']['sky']['code']),
+                       summary['tomorrow']['temperature']['tmin'], summary['tomorrow']['temperature']['tmax'])
 
     weather_report += "[모레의 날씨]\n(%s / 최저 온도 : %s도 , 최고 온도 : %s도)\n" % \
-                      (summary['dayAfterTomorrow']['sky']['name'], summary['dayAfterTomorrow']['temperature']['tmin'],
+                      (weather_status_with_emoji(summary['dayAfterTomorrow']['sky']['code']),
+                       summary['dayAfterTomorrow']['temperature']['tmin'],
                        summary['dayAfterTomorrow']['temperature']['tmax'])
 
     return weather_report
