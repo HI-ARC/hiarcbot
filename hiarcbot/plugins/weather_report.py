ceef4f10daccf4b56d2967e0fc3cabaed463d68d
KODING WARRIOR <rijgndqw012@gmail.com>
2018-04-29 16:59:05 +0900

    Slack Bot Major modification

--- hiarcbot/plugins/weather_report.py
+++ hiarcbot/plugins/weather_report.py
@@ -1,21 +1,17 @@
 import re
 import requests
+import operator
+
+import pyowm
 
 from slackbot.bot import respond_to
 
 from hiarcbot import bot, sched
 import slack_config
 
-url = 'https://api2.sktelecom.com/weather/summary'
-params = {
-    'version': 1,
-    'stnid': 108,  # 여의도 관측소
-}
-headers = {
-    'appKey': slack_config.WEATHER_PLANET_API_KEY,
-}
+API_KEY = slack_config.OPEN_WEATHER_API_KEY,
 
-
+'''
 def sky_code_to_message_with_emoji(code):
     mapping = {
         '01': '맑음 :sunny:',
@@ -30,37 +26,13 @@ def sky_code_to_message_with_emoji(code):
     status = code[-2:]
     msg = mapping.get(status, ':sunglasses:')
     return msg
-
-
-def basic_response():
-    return '[{day}의 날씨]\n({status} / 최저 온도 : {t_min}도 , 최고 온도 : {t_max}도)\n'
-
+'''
 
 def get_weather_summary():
-    r = requests.get(url=url, params=params, headers=headers)
-    if r.status_code != 200:
-        body = r.json().get('error')
-        code = body.get('code', '?')
-        msg = body.get.get('message', '아무튼 실패함')
-        return 'error code {}: {}'.format(code, msg)
-
-    summary = r.json()['weather']['summary'][0]
-    columns = {'today': '오늘', 'tomorrow': '내일', 'dayAfterTomorrow': '모레'}
-
-    def extract_message(day):
-        day_name = columns.get(day)
-        status = sky_code_to_message_with_emoji(summary[day]['sky']['code'])
-        temperature_min = summary[day]['temperature']['tmin']
-        temperature_max = summary[day]['temperature']['tmax']
-
-        msg = basic_response().format(
-            day=day_name,
-            status=status,
-            t_min=temperature_min,
-            t_max=temperature_max)
-        return msg
-
-    weather_report = '\n'.join(map(extract_message, columns))
+    f = open('/tmp/weather_report', 'r')
+    weather_report = f.read()
+    f.close()
+    
     return weather_report
 
 
@@ -69,6 +41,40 @@ def help(message):
     weather_report = get_weather_summary()
     message.send(weather_report)
 
+@sched.scheduled_job('cron', hour=0, minute=0)
+def save_weather_report():
+    weather_location = 'Yongsan,KR'
+
+    owm = pyowm.OWM(API_KEY)
+    fc = owm.three_hours_forecast(weather_location)
+    f = fc.get_forecast()
+
+    weathers = f.get_weathers()[:8]
+
+    status_frequency = {}
+    temps = []
+    status_arr = []
+    
+    for w in weathers:
+        temps.append(w.get_temperature(unit='celsius')['temp'])
+        temps.append(w.get_temperature(unit='celsius')['temp_max'])
+        temps.append(w.get_temperature(unit='celsius')['temp_min'])
+        status_arr.append(w.get_status())
+        status_frequency[w.get_status()] = 0
+        
+    for s in status_arr:
+        status_frequency[s] += 1
+
+    result = "오늘의 날씨 - 최저 기온 {}'C / 최고 기온 {}'C {}".format( \
+                                                               min(temps) \
+                                                               max(temps) \
+                                                               max(status_frequency.items(), key=operator.itemgetter(1))[0])
+
+    
+    with open('/tmp/weather_report', 'w') as f:
+        f.write(result)
+        f.close()
+        
 
 @sched.scheduled_job('cron', hour=7, minute=0)
 def notify_to_general_weather_summary():
